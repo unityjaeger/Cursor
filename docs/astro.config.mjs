f4152d5a38f1c8a3ba34be91f03a8342fe974fbc
unityjaeger <=>
2025-11-30 00:14:54 +0100

    add documentation

---
+++ docs/astro.config.mjs
@@ -0,0 +1,26 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		starlight({
+			title: 'Scrivener',
+			tableOfContents: {minHeadingLevel: 2, maxHeadingLevel: 4},
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/unityjaeger/Scrivener' }],
+			sidebar: [
+				{
+					label: 'Guides',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'Intro', slug: 'guides/intro' },
+						{ label: 'Usage', slug: 'guides/usage' },
+						{ label: 'Examples', slug: 'guides/examples' },
+						{ label: 'Custom Types', slug: 'guides/custom_types' }
+					],
+				}
+			],
+		}),
+	],
+});
